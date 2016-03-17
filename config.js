4ef3f2a0e1f246ab184bf24d06102d8a53587f5c
sjarosz <sjarosz@Stevens-MacBook-Pro.local>
2016-03-16 23:20:08 -0400

    update

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 /*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * If applicable, add the following below this MPL 2.0 HEADER, replacing
- * the fields enclosed by brackets "[]" replaced with your own identifying
- * information:
- *     Portions Copyright [yyyy] [name of copyright owner]
- *
- *     Copyright 2013 ForgeRock AS
+DISCLAIMER: The sample code described herein is provided on an "as is" basis, without 
+warranty of any kind, to the fullest extent permitted by law. ForgeRock does not warrant 
+or guarantee the individual success developers may have in implementing the sample code on 
+their development platforms or in production configurations.
+ForgeRock does not warrant, guarantee or make any representations regarding the 
+use, results of use, accuracy, timeliness or completeness of any data or information relating to the sample code. 
+ForgeRock disclaims all warranties, expressed or implied, and in particular, disclaims all warranties of merchantability, 
+and warranties related to the code, or any service or software related thereto.
+ForgeRock shall not be liable for any direct, indirect or consequential damages or costs of any type arising 
+out of any action taken by you or others related to the sample code.
+
  *
  */
 // START CONFIGURATION...
